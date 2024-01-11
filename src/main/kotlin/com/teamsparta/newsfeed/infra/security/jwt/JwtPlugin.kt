4495ded3bb85f5d20997933565162c76fc36f304
hongdul <kkw0426@naver.com>
2024-01-11 16:06:53 +0900

    Feat: add JwtPlugin, SecurityConfig
    - Jwt 기능 구현
    - filter control class 구현
    - application.yml 수정

---
+++ src/main/kotlin/com/teamsparta/newsfeed/infra/security/jwt/JwtPlugin.kt
@@ -0,0 +1,52 @@
+package com.teamsparta.newsfeed.infra.security.jwt
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jws
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import java.time.Duration
+import java.time.Instant
+import java.util.*
+
+@Component
+class JwtPlugin(
+    @Value("\${auth.jwt.issuer}") private val issuer: String,
+    @Value("\${auth.jwt.secret}") private val secret: String,
+    @Value("\${auth.jwt.accessTokenExpirationHour}") private val accessTokenExpirationHour: Long,
+) {
+    // kotlin에서는 try catch 대신 Result 형태로 exception 처리 가능
+    fun validateToken(jwt: String): Result<Jws<Claims>> {
+        return kotlin.runCatching {
+            val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
+            // key로 서명 검증, 만료시간 체크
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(jwt)
+        }
+    }
+
+    fun generateAccessToken(subject: String, email: String, role: String): String {
+        // subject, 만료기간과 role을 설정합니다.
+        return generateToken(subject, email, role, Duration.ofHours(accessTokenExpirationHour))
+    }
+
+    private fun generateToken(subject: String, email: String, role: String, expirationPeriod : Duration): String {
+        // custom claim을 설정합니다.
+        val claims: Claims = Jwts.claims()
+            .add(mapOf("role" to role, "email" to email))
+            .build()
+
+        val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
+        val now = Instant.now()
+
+        return Jwts.builder()
+            .subject(subject)
+            .issuer(issuer)
+            .issuedAt(Date.from(now))
+            .expiration(Date.from(now.plus(expirationPeriod)))
+            .claims(claims)
+            .signWith(key)
+            .compact()
+    }
+}
