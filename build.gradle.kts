eff897eba811c93479c7371fc759a0d6f02cd776
hongdul <kkw0426@naver.com>
2024-01-11 14:38:11 +0900

    Feat: add dependency, add spring security log
    - Spring 심화 강의안 중
      - dependency에 spring-security 추가,jwt 라이브러리 - jjwt 추가
      - application.yml에 spring security log 볼 수 있게 코드 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,15 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//    implementation("com.h2database:h2")
+
+
+    // Spring Security 심화 강의안- jwt 관련 라이브러리
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+    // -------------------
 
     runtimeOnly("org.postgresql:postgresql")
 
